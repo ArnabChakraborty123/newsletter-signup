d66f182bd544431e47d107c3adca858c79a5f282
ArnabChakraborty123 <arnab.chakraborty.ece25@heritageit.edu.in>
2023-06-18 17:27:12 +0530

    fixed

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ const https = require("https");
 
 const app = express();
 
-app.use(express.static("public"));// providing the path of our static files,then we should be refer to these static files by relative URL   , and that is relative to public folder.
+app.use(express.static(__dirname + "/public/"));
+// providing the path of our static files,then we should be refer to these static files by relative URL   , and that is relative to public folder.
 app.use(bodyParser.urlencoded({extended:true}));
 app.get("/",function(req,res){
     res.sendFile(__dirname + "/signup.html");
